3f3da1d4d4a8aa74ea4325c9f1ac2c984f8cdf6a
Wel Marquez <wlmrqz@protonmail.com>
2020-02-02 14:02:26 +0800

    create dummy user data

--- src/sandbox/testPost.js
+++ src/sandbox/testPost.js
@@ -1,12 +1,37 @@
 const response = require('../commons/response');
 const health = require('../commons/health');
+const User = require('../models/User');
 
 // eslint-disable-next-line no-unused-vars
 module.exports.handler = async (event, context) => {
   try {
-    const params = JSON.parse(event.body);
-    const result = health.buildMeasurements(params);
-    return response.buildSuccess(result);
+    const params = event.pathParameters;
+    console.log('PARAMS:', params);
+
+    if (!params  || !params.userProfile) {
+      return response.buildError(401);
+    }
+
+    const user = await User.findByPk(params.userProfile);
+    console.log('USER:', user);
+
+    if (!user) {
+      return response.buildError(401);
+    }
+
+    const body = JSON.parse(event.body);
+
+    if (!body) {
+      return response.buildError(400);
+    }
+
+    const result = health.buildMeasurements(body);
+
+    // TODO:
+    // Implement insert for the
+    // built measurements.
+
+    return response.buildSuccess(result, 201);
   } catch (error) {
     console.error('ERROR:', error);
     return response.buildError(500);
