c5ab52c030b0fad12ded6fcd900b4c0b8ae2bfb4
Joel Marquez <joel@expertcore.net>
2020-02-04 22:39:49 +0800

    use N. Virginia region

--- src/handlers/getMeasurement.js
+++ src/handlers/getMeasurement.js
@@ -1,8 +1,16 @@
+const authenticate = require('../commons/authenticate');
 const response = require('../commons/response');
 
 // eslint-disable-next-line no-unused-vars
 module.exports.handler = async (event, context) => {
   try {
+    // Validate `userProfile` parameter.
+    const user = await authenticate.userProfile(event.pathParameters);
+
+    if (!user) {
+      return response.buildError(401);
+    }
+
     return response.buildSuccess({
       id: 123,
       dateTime: '2020-01-16T19:19:30Z',
